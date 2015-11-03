8e9beffd0955bc10ef0cc7367671ff22da91710d
srf5132 <srf5132@gmail.com>
2015-11-02 21:37:21 -0500

    Update blocking_queue.py

--- blocking_queue.py
+++ blocking_queue.py
@@ -1,23 +1,31 @@
-from threading import Lock 
- 
-class blocking_queue(): 
-        x = 1 
-        list = [] 
-        lock = Lock() 
-        def put(self,y): 
-                self.lock.acquire() 
-                self.list.append(y) 
-                self.lock.release() 
-                return self.list[0] 
-        def get(self): 
-                self.lock.acquire() 
-                if(len(self.list) > 0): 
-                        a = self.list 
-                        self.list = [] 
-                self.lock.release() 
-                return a 
-        def get_size(self): 
-                self.lock.acquire() 
-                a = len(self.list) 
-                self.lock.release() 
-                return a
+from threading import Lock
+
+class blocking_queue:
+	x = 1
+	list = []
+	lock = Lock()
+	threshold=0
+	def __init__(self,threshold):
+		self.threshold=threshold
+	def put(self,y):
+		self.lock.acquire()
+		if(len(self.list) < self.threshold):
+			self.list.append(y)
+		else:
+			self.list.append(y)
+			self.list.pop()
+		self.lock.release()
+		return
+	def get(self):
+		self.lock.acquire()
+		a = []
+		if(len(self.list) >=  self.threshold):
+			a = self.list
+			self.list = []
+		self.lock.release()
+		return a
+	def get_size(self):
+		self.lock.acquire()
+		a = len(self.list)
+		self.lock.release()
+		return a
